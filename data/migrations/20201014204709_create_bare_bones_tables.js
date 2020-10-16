26598fd19c9f315d883ce94436b0e3d5db4cd912
Shadewalker1984 <Shadewalker1984@yahoo.com>
2020-10-15 19:29:34 -0500

    built bare bones tables for 8 of 8 tables. Subject to change when filling out the data

--- data/migrations/20201014204709_create_bare_bones_tables.js
+++ data/migrations/20201014204709_create_bare_bones_tables.js
@@ -10,20 +10,49 @@ exports.up = async (knex) => {
     .createTable('customer', (table) => {
       table.string('id').notNullable().unique().primary();
       table.string('name');
-  })
+    });
+  await knex.schema
+    .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+    .createTable('pet_types', (table) => {
+      table.string('breed').notNullable();
+    });
+  await knex.schema
+    .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+    .createTable('pets', (table) => {
+      table.string('id').notNullable().unique().primary();
+      table.string('name').notNullable()
+    });
   await knex.schema
     .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
     .createTable('services', (table) => {
       table.string('id').notNullable().unique().primary();
       table.string('service_name').notNullable()
-    })
+    });
   await knex.schema
-  .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
-  .createTable('groomer_services', (table) => {
-    table.string('groomer_id').references('id').inTable('groomers')
-    table.string('services_id').references('id').inTable('services')
-    table.int('services_price')
+    .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+    .createTable('groomer_services', (table) => {
+      table.string('groomer_id').references('id').inTable('groomers')
+      table.string('services_id').references('id').inTable('services')
+      table.int('services_price')
+    });
+  await knex.schema
+    .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+    .createTable('customer_pets_type', (table) => {
+      table.string('customer_id').references('id').inTable('customer')
+      table.string('pets_id').references('id').inTable('pets')
+      table.string('pet_types').references('id').inTable('pet_types')
+    })
+  await knex.shema.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"').createTable('groomer_schedule', (table) => {
+    table.string('id')
   })
 };
 
-exports.down = function (knex) {};
+exports.down = function (knex) {
+  await knex.schema.dropTableIfExists("customer_pets_type");
+  await knex.schema.dropTableIfExists("groomer_services");
+  await knex.schema.dropTableIfExists("services");
+  await knex.schema.dropTableIfExists("pets");
+  await knex.schema.dropTableIfExists("pet_types");
+  await knex.schema.dropTableIfExists("customer");
+  await knex.schema.dropTableIfExists('groomer')
+};
