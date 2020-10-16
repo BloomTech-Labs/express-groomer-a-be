5f6ea3ece4f2deb607ddbd55f8d528f60d7dd3fb
Shadewalker1984 <Shadewalker1984@yahoo.com>
2020-10-16 18:06:52 -0500

    fixed minor typo

--- data/migrations/20201014204709_create_bare_bones_tables.js
+++ data/migrations/20201014204709_create_bare_bones_tables.js
@@ -1,53 +1,37 @@
 exports.up = async (knex) => {
-  await knex.schema
-    // .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
-    .createTable('groomer', (table) => {
-      table.increments('id');
-      table.string('name');
-    });
-  await knex.schema
-    // .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
-    .createTable('customer', (table) => {
-      table.increments('id');
-      table.string('name');
-    });
-  await knex.schema
-    // .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
-    .createTable('pet_types', (table) => {
-      table.increments('id');
-      table.string('breed');
-    });
-  await knex.schema
-    // .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
-    .createTable('pets', (table) => {
-      table.increments('id');
-      table.bigint('pet_types_id').references('id').inTable('pet_types');
-      table.string('name').notNullable();
-    });
-  await knex.schema
-    // .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
-    .createTable('services', (table) => {
-      table.increments('id');
-      table.string('service_name').notNullable();
-    });
-  await knex.schema
-    // .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
-    .createTable('groomer_services', (table) => {
-      table.bigint('groomer_id').references('id').inTable('groomer');
-      table.bigint('services_id').references('id').inTable('services');
-      table.bigint('services_price');
-    });
-  await knex.schema
-    // .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
-    .createTable('customer_pets_type', (table) => {
-      table.bigint('customer_id').references('id').inTable('customer');
-      table.bigint('pets_id').references('id').inTable('pets');
-    });
-  await knex.schema
-    // .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
-    .createTable('groomer_schedule', (table) => {
-      table.increments('id');
-    });
+  await knex.schema.createTable('groomer', (table) => {
+    table.increments('id');
+    table.string('name');
+  });
+  await knex.schema.createTable('customer', (table) => {
+    table.increments('id');
+    table.string('name');
+  });
+  await knex.schema.createTable('pet_types', (table) => {
+    table.increments('id');
+    table.string('breed');
+  });
+  await knex.schema.createTable('pets', (table) => {
+    table.increments('id');
+    table.bigint('pet_types_id').references('id').inTable('pet_types');
+    table.string('name').notNullable();
+  });
+  await knex.schema.createTable('services', (table) => {
+    table.increments('id');
+    table.string('service_name').notNullable();
+  });
+  await knex.schema.createTable('groomer_services', (table) => {
+    table.bigint('groomer_id').references('id').inTable('groomer');
+    table.bigint('services_id').references('id').inTable('services');
+    table.bigint('services_price');
+  });
+  await knex.schema.createTable('customer_pets_type', (table) => {
+    table.bigint('customer_id').references('id').inTable('customer');
+    table.bigint('pets_id').references('id').inTable('pets');
+  });
+  await knex.schema.createTable('groomer_schedule', (table) => {
+    table.increments('id');
+  });
 };
 
 exports.down = async function (knex) {
