d98635c87531abf0456171b72ac63e701b383e27
Shadewalker1984 <Shadewalker1984@yahoo.com>
2020-10-15 20:40:06 -0500

    barebones struture of the backend is finished.  Ready for table data to be filled in by the corresponding developer dealing with that table.  Several Seeds also supplied for structure/syntax/pattern use

--- data/migrations/20201014204709_create_bare_bones_tables.js
+++ data/migrations/20201014204709_create_bare_bones_tables.js
@@ -1,58 +1,59 @@
 exports.up = async (knex) => {
   await knex.schema
-    .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+    // .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
     .createTable('groomer', (table) => {
-      table.string('id').notNullable().unique().primary();
+      table.increments("id");
       table.string('name');
     });
   await knex.schema
-    .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+    // .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
     .createTable('customer', (table) => {
-      table.string('id').notNullable().unique().primary();
+      table.increments("id");
       table.string('name');
     });
   await knex.schema
-    .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+    // .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
     .createTable('pet_types', (table) => {
-      table.string('id').notNullable().unique().primary()
-      table.string('breed').notNullable();
+      table.increments("id");
+      table.string('breed');
     });
   await knex.schema
-    .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+    // .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
     .createTable('pets', (table) => {
-      table.string('id').notNullable().unique().primary();
+      table.increments("id");
+      table.bigint('pet_types_id').references('id').inTable('pet_types')
       table.string('name').notNullable()
     });
   await knex.schema
-    .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+    // .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
     .createTable('services', (table) => {
-      table.string('id').notNullable().unique().primary();
+      table.increments("id");
       table.string('service_name').notNullable()
     });
   await knex.schema
-    .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+    // .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
     .createTable('groomer_services', (table) => {
-      table.string('groomer_id').references('id').inTable('groomer')
-      table.string('services_id').references('id').inTable('services')
-      table.string('services_price');
+      table.bigint('groomer_id').references('id').inTable('groomer')
+      table.bigint('services_id').references('id').inTable('services')
+      table.bigint('services_price');
     });
   await knex.schema
-    .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+    // .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
     .createTable('customer_pets_type', (table) => {
-      table.string('customer_id').references('id').inTable('customer')
-      table.string('pets_id').references('id').inTable('pets')
-      table.string('pet_types').references('id').inTable('pet_types')
+      table.bigint('customer_id').references('id').inTable('customer')
+      table.bigint('pets_id').references('id').inTable('pets')
     })
   await knex.schema
-    .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+    // .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
     .createTable('groomer_schedule', (table) => {
-      table.string('id')
+      table.increments("id");
     })
 };
 
 exports.down = async function (knex) {
   await knex.schema.dropTableIfExists("customer_pets_type");
   await knex.schema.dropTableIfExists("groomer_services");
+  await knex.schema.dropTableIfExists('groomer_schedule');
   await knex.schema.dropTableIfExists("services");
   await knex.schema.dropTableIfExists("pets");
   await knex.schema.dropTableIfExists("pet_types");
--- data/seeds/001_profiles.js
+++ data/seeds/001_profiles.js
@@ -8,6 +8,7 @@ const profiles = [...new Array(5)].map((i, idx) => ({
     idx === 0
       ? 'Test001 User'
       : `${faker.name.firstName()} ${faker.name.lastName()}`,
+  role: "None"
 }));
 
 exports.seed = function (knex) {
