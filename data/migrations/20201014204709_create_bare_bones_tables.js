680351a50cd4e53c8488d34cf925141f6ca6f5bf
Josh Riddle <riddlej9879@yahoo.com>
2021-01-11 20:07:07 -0600

    Fixed typo for pull

--- data/migrations/20201014204709_create_bare_bones_tables.js
+++ data/migrations/20201014204709_create_bare_bones_tables.js
@@ -3,8 +3,7 @@
 // 
 
 exports.up = async (knex) => {
-  // DONE
-  await knex.schema.createTable('groomer', (table) => {
+  await knex.schema.createTable('groomers', (table) => {
     table.increments('id');
     table.string('user_id').unique().references('id').inTable('profiles');
     table.string('business_name');
@@ -19,10 +18,10 @@ exports.up = async (knex) => {
     table.varchar('email').notNull();
     table.varchar('about', 5000);
     table.varchar('hours', 5000);
+    table.string('avatarUrl');
   });
 
-  // DONE
-  await knex.schema.createTable('customer', (table) => {
+  await knex.schema.createTable('customers', (table) => {
     table.increments('id');
     table.string('user_id').unique().references('id').inTable('profiles');
     table.string('given_name').notNull();
@@ -33,9 +32,9 @@ exports.up = async (knex) => {
     table.varchar('state');
     table.varchar('zip_code');
     table.varchar('country');
+    table.string('avatarUrl');
   });
 
-  // DONE
   await knex.schema.createTable('pets', (table) => {
     table.increments('id');
     table.string('customer_id').references('user_id').inTable('customers');
@@ -48,37 +47,23 @@ exports.up = async (knex) => {
     table.varchar('special_requests', 5000);
     table.varchar('pet_temperament', 500);
     table.boolean('shots_current').notNull();
+    table.string('avatarUrl');
   });
 
-  // DONE
   await knex.schema.createTable('services', (table) => {
     table.increments('id');
     table.string('service_name').notNull();
   });
 
-  // DONE
   await knex.schema.createTable('groomer_services', (table) => {
     table.string('groomer_id').references('user_id').inTable('groomers');
     table.bigint('services_id').references('id').inTable('services');
     table.decimal('services_price', 14, 2);
   });
-
-  // THIS TABLE IS OBSOLETE
-  await knex.schema.createTable('customer_pets_type', (table) => {
-    table.string('customer_id').references('user_id').inTable('customers');
-    table.bigint('pets_id').references('id').inTable('pets');
-  });
-
-  // THIS TABLE IS OBSOLETE
-  await knex.schema.createTable('groomer_schedule', (table) => {
-    table.increments('id');
-  });
 };
 
 exports.down = async function (knex) {
-  await knex.schema.dropTableIfExists('customer_pets_type');
   await knex.schema.dropTableIfExists('groomer_services');
-  await knex.schema.dropTableIfExists('groomer_schedule');
   await knex.schema.dropTableIfExists('services');
   await knex.schema.dropTableIfExists('pets');
   await knex.schema.dropTableIfExists('customer');
