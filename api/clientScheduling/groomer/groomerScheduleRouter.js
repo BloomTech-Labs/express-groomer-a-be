37460f1fff730e0f23b94085e21f82759fc3db34
Kaleb Johnson <KalebJohnson45@yahoo.com>
2021-03-09 19:52:08 -0600

    lint

--- api/clientScheduling/groomer/groomerScheduleRouter.js
+++ api/clientScheduling/groomer/groomerScheduleRouter.js
@@ -2,36 +2,36 @@ const schedule = require('../schedulingModel');
 const router = require('express').Router({ mergeParams: true });
 
 router.all('/', function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', process.env.CORS_ORIGIN);
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
-    next();
+  res.header('Access-Control-Allow-Origin', process.env.CORS_ORIGIN);
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  next();
 });
 
 /******************************************************************************
  *                      GET ALL appointments by groomer_id
  ******************************************************************************/
 router.get('/', async (req, res) => {
-    const { id: groom_id } = req.params;
-    const appointments = await schedule.getAppointmentsGroomer(groom_id);
-    const validGroomer = await schedule.validGroomer(groom_id);
-    if (!validGroomer.length) {
-        res.status(400).json({ error: `No groomer found by ID: ${groom_id}` });
-    } else {
-        res.status(200).json(
-            await Promise.all(
-                appointments.map(async (appointment) =>
-                    schedule
-                        .getAppointmentItems(appointment.transaction)
-                        .then((services) => {
-                            formattedServices = services.map(
-                                (service) => service.service_name
-                            );
-                            return { ...appointment, ...{ transaction: formattedServices } };
-                        })
-                )
-            )
-        );
-    }
+  const { id: groom_id } = req.params;
+  const appointments = await schedule.getAppointmentsGroomer(groom_id);
+  const validGroomer = await schedule.validGroomer(groom_id);
+  if (!validGroomer.length) {
+    res.status(400).json({ error: `No groomer found by ID: ${groom_id}` });
+  } else {
+    res.status(200).json(
+      await Promise.all(
+        appointments.map(async (appointment) =>
+          schedule
+            .getAppointmentItems(appointment.transaction)
+            .then((services) => {
+              formattedServices = services.map(
+                (service) => service.service_name
+              );
+              return { ...appointment, ...{ transaction: formattedServices } };
+            })
+        )
+      )
+    );
+  }
 });
 
 /******************************************************************************
@@ -39,30 +39,30 @@ router.get('/', async (req, res) => {
  ******************************************************************************/
 
 router.get('/:customer_id', async (req, res) => {
-    const { id: groom_id, customer_id } = req.params;
-    const appointments = await schedule.findAppointmentsByRelation(
-        customer_id,
-        groom_id
+  const { id: groom_id, customer_id } = req.params;
+  const appointments = await schedule.findAppointmentsByRelation(
+    customer_id,
+    groom_id
+  );
+  const validGroomer = await schedule.validGroomer(groom_id);
+  if (!validGroomer.length) {
+    res.status(400).json({ error: `No groomer found by ID: ${groom_id}` });
+  } else {
+    res.status(200).json(
+      await Promise.all(
+        appointments.map(async (appointment) =>
+          schedule
+            .getAppointmentItems(appointment.transaction)
+            .then((services) => {
+              formattedServices = services.map(
+                (service) => service.service_name
+              );
+              return { ...appointment, ...{ transaction: formattedServices } };
+            })
+        )
+      )
     );
-    const validGroomer = await schedule.validGroomer(groom_id);
-    if (!validGroomer.length) {
-        res.status(400).json({ error: `No groomer found by ID: ${groom_id}` });
-    } else {
-        res.status(200).json(
-            await Promise.all(
-                appointments.map(async (appointment) =>
-                    schedule
-                        .getAppointmentItems(appointment.transaction)
-                        .then((services) => {
-                            formattedServices = services.map(
-                                (service) => service.service_name
-                            );
-                            return { ...appointment, ...{ transaction: formattedServices } };
-                        })
-                )
-            )
-        );
-    }
+  }
 });
 
 /******************************************************************************
@@ -70,43 +70,44 @@ router.get('/:customer_id', async (req, res) => {
  ******************************************************************************/
 
 router.put('/confirm/:customer_id', async (req, res) => {
-    try {
-        const { id: groom_id, customer_id } = req.params;
-        const { confirmation, start } = req.body;
+  try {
+    const { id: groom_id, customer_id } = req.params;
+    const { confirmation, start } = req.body;
 
-        if (!groom_id || !customer_id || !start) {
-            return res.status(400).json({
-                message:
-                    'Groomer id, customer id, appointment start time, and confirmation required!',
-            });
-        }
-
-        if (typeof confirmation != 'boolean') {
-            return res
-                .status(400)
-                .json({ message: 'Confirmation requires a boolean value' });
-        }
-
-        const data = await schedule.findAppointmentsByRelation(
-            customer_id,
-            groom_id
-        );
-
-        if (data.length) {
-            await schedule.updateConfirmation(
-                customer_id,
-                groom_id,
-                start,
-                confirmation
-            );
-            return res.status(200).json({
-                message: `${confirmation ? 'appoinment accepted!' : 'appointment declined!'
-                    }`,
-            });
-        }
-    } catch (err) {
-        res.status(500).json({ message: err.message });
+    if (!groom_id || !customer_id || !start) {
+      return res.status(400).json({
+        message:
+          'Groomer id, customer id, appointment start time, and confirmation required!',
+      });
     }
+
+    if (typeof confirmation != 'boolean') {
+      return res
+        .status(400)
+        .json({ message: 'Confirmation requires a boolean value' });
+    }
+
+    const data = await schedule.findAppointmentsByRelation(
+      customer_id,
+      groom_id
+    );
+
+    if (data.length) {
+      await schedule.updateConfirmation(
+        customer_id,
+        groom_id,
+        start,
+        confirmation
+      );
+      return res.status(200).json({
+        message: `${
+          confirmation ? 'appoinment accepted!' : 'appointment declined!'
+        }`,
+      });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
 module.exports = router;
--- data/migrations/0009_client-scheduling.js
+++ data/migrations/0009_client-scheduling.js
@@ -1,27 +1,27 @@
 exports.up = async (knex) => {
-    await knex.schema.createTable('scheduling', (table) => {
-        table.increments('transaction').primary();
-        table
-            .string('customer_id')
-            .references('user_id')
-            .inTable('customer')
-            .onDelete('cascade')
-            .onUpdate('cascade')
-            .notNull();
-        table
-            .string('groom_id')
-            .references('user_id')
-            .inTable('groomer')
-            .onDelete('cascade')
-            .onUpdate('cascade')
-            .notNull();
-        table.boolean('confirmation').defaultTo(null);
-        table.date('date', { useTz: false }).notNull();
-        table.time('startTime').notNull();
-        table.time('endTime').notNull();
-    });
+  await knex.schema.createTable('scheduling', (table) => {
+    table.increments('transaction').primary();
+    table
+      .string('customer_id')
+      .references('user_id')
+      .inTable('customer')
+      .onDelete('cascade')
+      .onUpdate('cascade')
+      .notNull();
+    table
+      .string('groom_id')
+      .references('user_id')
+      .inTable('groomer')
+      .onDelete('cascade')
+      .onUpdate('cascade')
+      .notNull();
+    table.boolean('confirmation').defaultTo(null);
+    table.date('date', { useTz: false }).notNull();
+    table.time('startTime').notNull();
+    table.time('endTime').notNull();
+  });
 };
 
 exports.down = async (knex) => {
-    await knex.schema.dropTableIfExists('scheduling');
+  await knex.schema.dropTableIfExists('scheduling');
 };
