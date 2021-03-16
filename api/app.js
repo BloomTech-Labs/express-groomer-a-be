c54db09f8df54639519eda8bab37785fbfc6d47b
Aaron M. Verdine <a.m.verdine@gmail.com>
2021-03-15 20:06:03 -0400

    Feature/appt notification (#6)
    * Setup back-end // Create appointments folder, router & model files // Add appointmentRouter to app.js
    
    * Install JSON parser dependencies // Install Twilio // Setup JSON parsers and Twilio
    
    * Install Twilio dev dependency // Set Twilio credentials in .env
    
    * Setup JSON parsers // Build POST request that will be used to send SMS messages from Twilio phone number
    
    * Fixed App.js export error
    
    * Added express.Router()
    
    * Reset routers back to original
    
    * Comment out appointmentRouter
    
    * Update PR
    
    * Update app.js
    
    Co-authored-by: Jarrod Skahill <54871000+jarrod847@users.noreply.github.com>
    Co-authored-by: Blake Davis <58578456+davisblakep@users.noreply.github.com>

--- api/app.js
+++ api/app.js
@@ -9,6 +9,13 @@ const swaggerJSDoc = require('swagger-jsdoc');
 const jsdocConfig = require('../config/jsdoc');
 const dotenv = require('dotenv');
 const config_result = dotenv.config();
+const bodyParser = require('body-parser');
+const pino = require('express-pino-logger')();
+const client = require('twilio')(
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_AUTH_TOKEN
+);
+
 if (process.env.NODE_ENV != 'production' && config_result.error) {
   throw config_result.error;
 }
@@ -26,6 +33,8 @@ const groomerRouter = require('./groomers/groomerRouter');
 const servicesRouter = require('./services/servicesRouter');
 const groomerServicesRouter = require('./groomerservices/groomerServicesRouter');
 const petRouter = require('./pets/petsRouter');
+const ratingsRouter = require('./Ratings/ratingsRouter');
+// const appointmentRouter = require('./appointments/appointmentRouter');
 
 //const favoritesRouter = require('./customer_favs/customerFavRouter')
 
@@ -55,6 +64,11 @@ app.use(logger('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+// Twilio JSON Parsers
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(pino);
+
 // application routes
 app.use('/', indexRouter);
 app.use(['/profile', '/profiles'], profileRouter);
@@ -63,6 +77,10 @@ app.use('/groomers', groomerRouter);
 app.use(['/services', '/services'], servicesRouter);
 app.use(['/groomerservices', '/groomer_services'], groomerServicesRouter);
 app.use('/pets', petRouter);
+app.use('/ratings', ratingsRouter);
+// app.use('/appointments', appointmentRouter);
+
+//app.use('/favorites', favoritesRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -89,6 +107,30 @@ app.use(function (err, req, res, next) {
     return res.json(errObject);
   }
   next(err);
+});
+
+// Twilio Requests
+app.get('/api/greeting', (req, res) => {
+  const name = req.query.name || 'World';
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
+});
+
+app.post('/api/messages', (req, res) => {
+  res.header('Content-Type', 'application/json');
+  client.messages
+    .create({
+      from: process.env.TWILIO_PHONE_NUMBER,
+      to: req.body.to,
+      body: req.body.body,
+    })
+    .then(() => {
+      res.send(JSON.stringify({ success: true }));
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send(JSON.stringify({ success: false }));
+    });
 });
 
 module.exports = app;
