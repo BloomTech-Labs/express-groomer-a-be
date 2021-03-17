704554824eb9fa5c9be2edc6da2c0327ff3a3443
Kaleb Johnson <58450152+KalebJohnson@users.noreply.github.com>
2021-03-17 18:16:45 -0500

    Some simple comment functionality for ratings/reviews (#9)
    * Some simple comment functionality for ratings/reviews
    
    * Omg lint

--- data/seeds/008_ratings.js
+++ data/seeds/008_ratings.js
@@ -4,16 +4,19 @@ exports.seed = async function (knex) {
       customer_id: '00ultwz1n9ORpNFc04x6',
       groom_id: '00ultx74kMUmEW8054x6',
       rate: 1,
+      comment: 'They lost my freakin dog!',
     },
     {
       customer_id: '00ultwz1n9ORpNFc04x6',
       groom_id: '00ultwew80Onb2vOT4x6',
       rate: 5,
+      comment: 'My dog is now the Brad Pitt of dogs!',
     },
     {
       customer_id: '00u13omswyZM1xVya4x7',
       groom_id: '00ultwew80Onb2vOT4x6',
       rate: 3,
+      comment: 'Meh..',
     },
   ]);
 };
