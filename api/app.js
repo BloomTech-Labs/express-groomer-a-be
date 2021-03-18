d8abe808b1b7ef5679863c3be534353a687c76d0
Your name <a.m.verdine@gmail.com>
2021-03-17 22:23:36 -0400

    update changes

--- api/app.js
+++ api/app.js
@@ -109,11 +109,11 @@ app.use(function (err, req, res, next) {
 });
 
 // Twilio Requests
-app.get('/api/greeting', (req, res) => {
-  const name = req.query.name || 'World';
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
-});
+// app.get('/api/greeting', (req, res) => {
+//   const name = req.query.name || 'World';
+//   res.setHeader('Content-Type', 'application/json');
+//   res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
+// });
 
 app.post('/api/messages', (req, res) => {
   res.header('Content-Type', 'application/json');
